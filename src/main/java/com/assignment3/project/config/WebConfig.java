067c1a2c30b68cc25498c8fe959956f7f5f7672b
jooke <zhaniyakraman@gmail.com>
2025-11-21 16:27:17 +0500

    EnableWebMvc fix CORS

--- src/main/java/com/assignment3/project/config/WebConfig.java
+++ src/main/java/com/assignment3/project/config/WebConfig.java
@@ -3,10 +3,12 @@ package com.assignment3.project.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig {
 
     @Bean
