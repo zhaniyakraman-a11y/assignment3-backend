61bf9076fb795a7289ece195ab3bbb5e0f9e6d45
jooke <zhaniyakraman@gmail.com>
2025-12-02 22:42:55 +0500

    site

--- src/main/java/com/assignment3/project/security/SecurityConfig.java
+++ src/main/java/com/assignment3/project/security/SecurityConfig.java
@@ -140,6 +140,7 @@ public class SecurityConfig {
         // Разрешаем запросы с локального окружения и Netlify
         configuration.setAllowedOrigins(List.of(
             "http://localhost:5173",
+            "https://azharfund.netlify.app/",
             "https://assignment3-vue.netlify.app"
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
